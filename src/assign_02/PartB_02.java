aafce041f671450d03e121e1008ab8811251ceac
Alan Treanor <alan.treanor@ucdconnect.ie>
2019-03-22 11:16:05 +0000

    AdvancedDataStructuresInJava - PartB_02

---
+++ src/assign_02/PartB_02.java
@@ -0,0 +1,22 @@
+package assign_02;
+
+/*
+ * Class to calculate & return the average of two integers
+ */
+public class PartB_02 {
+	
+	public static void main(String[] args) {
+		// invoke Average method on parameters
+		Average(20, 80);
+	}
+	public static int Average(int a, int b) {
+		
+		// calculate average of input parameters
+		int avg = (a + b) / 2;
+		
+		// print average to console & return average
+		System.out.println("Output: " + avg);
+		return avg;
+	}
+
+}
