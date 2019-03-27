8ac198f4bb01db92c4d68914114022224a695ff1
Alan Treanor <alan.treanor@ucdconnect.ie>
2019-03-27 08:13:15 +0000

    AdvancedDataStructuresInJava - PartB_03

---
+++ src/assign_02/PartB_03.java
@@ -0,0 +1,26 @@
+package assign_02;
+
+/*
+ * Class to calculate & return the maximum of two integers
+ */
+public class PartB_03 {
+	
+	public static void main(String[] args) {
+		// invoke MaxInt on parameters
+		MaxInt(10, 20);
+	}
+	
+	public static int MaxInt(int a, int b) {
+		// if a > b, print output a to console & return a
+		if (a > b) {
+			System.out.println("Output: " + a);
+			return a;
+		} 
+		// if b > a, print output b to console & return b
+		else {
+			System.out.println("Output: " + b);
+			return b;
+		}
+	}
+
+}
